b2103fbce0d53c160b9b4da8a4f780d757fa4477
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2018-10-27 14:20:21 +0200

    boutons accueil réalisés

--- MonopolyGenetiqueJava/src/IHM/Fenetre/Accueil.java
+++ MonopolyGenetiqueJava/src/IHM/Fenetre/Accueil.java
@@ -1,4 +1,35 @@
 package IHM.Fenetre;
 
-public class Accueil {
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+
+
+public class Accueil extends Parent {
+
+    public Accueil(){
+        Button solo = new Button("Solo") ;
+        Button HumainVShumain = new Button("Humain VS Humain");
+        Button iaVSia = new Button("IA VS IA");
+
+        solo.setLayoutX(50);
+        solo.setLayoutY(20);
+
+        HumainVShumain.setLayoutX(50);
+        HumainVShumain.setLayoutY(50);
+
+        iaVSia.setLayoutX(50);
+        iaVSia.setLayoutY(80);
+
+        this.setTranslateX(20);
+        this.setTranslateY(20);
+
+        this.getChildren().add(solo);
+        this.getChildren().add(HumainVShumain);
+        this.getChildren().add(iaVSia);
+    }
+
+
+
 }
+
+
--- MonopolyGenetiqueJava/src/sample/Main.java
+++ MonopolyGenetiqueJava/src/sample/Main.java
@@ -1,7 +1,9 @@
 package sample;
 
+import IHM.Fenetre.Accueil;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,9 +12,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
         primaryStage.setTitle("Monopoly Génétique");
+        Group root = new Group() ;
+
+        Accueil accueil = new Accueil() ;
+
         primaryStage.setScene(new Scene(root, 300, 275));
+
+        root.getChildren().add(accueil) ;
         primaryStage.show();
     }
 
