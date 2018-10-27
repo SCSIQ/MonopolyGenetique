c933f90d5bd9bbb6ea44e4e8c70ec2fb6106b427
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2018-10-27 14:26:04 +0200

    hgfds

--- MonopolyGenetiqueJava/src/IHM/Fenetre/Accueil.java
+++ MonopolyGenetiqueJava/src/IHM/Fenetre/Accueil.java
@@ -1,5 +1,7 @@
 package IHM.Fenetre;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 
@@ -11,6 +13,7 @@ public class Accueil extends Parent {
         Button HumainVShumain = new Button("Humain VS Humain");
         Button iaVSia = new Button("IA VS IA");
 
+        //POSITION DES BOUTONS
         solo.setLayoutX(50);
         solo.setLayoutY(20);
 
@@ -20,9 +23,35 @@ public class Accueil extends Parent {
         iaVSia.setLayoutX(50);
         iaVSia.setLayoutY(80);
 
+        //ACTION : SI ON APPUIE SUR SOLO
+        solo.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+
+        //ACTION : SI ON APPUIE SUR HumainVShumain
+        HumainVShumain.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+
+        //ACTION : SI ON APPUIE SUR iaVSia
+        iaVSia.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+
         this.setTranslateX(20);
         this.setTranslateY(20);
 
+
+        // AJOUT A L'ECRAN
         this.getChildren().add(solo);
         this.getChildren().add(HumainVShumain);
         this.getChildren().add(iaVSia);
