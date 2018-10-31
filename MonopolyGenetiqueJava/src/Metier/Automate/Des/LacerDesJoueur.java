d75c1c9ee7cfdf9e46ae12af4f1d50bfb626369b
Aurian <durand.aurian@me.com>
2018-10-31 15:18:13 +0100

    Update LancerDesJoueur

--- MonopolyGenetiqueJava/src/Metier/Automate/Des/LacerDesJoueur.java
+++ MonopolyGenetiqueJava/src/Metier/Automate/Des/LacerDesJoueur.java
@@ -1,7 +1,24 @@
 package Metier.Automate.Des;
 
-public class LacerDesJoueur {
+import Entites.Joueur;
+import Metier.Automate.Etat;
+
+public class LacerDesJoueur extends Etat {
+
+    public LacerDesJoueur(Joueur joueur) {
+        super(joueur);
+    }
 
     public void LancementDes(){
+    }
+
+    @Override
+    public void agir() {
+
+    }
+
+    @Override
+    public Etat transition() {
+        return null;
     }
 }
