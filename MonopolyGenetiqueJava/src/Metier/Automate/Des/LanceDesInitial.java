9c96ec4cdcc11c1da2d30937e4f9e880689aa73f
Papydragon <43467150+Papydragon@users.noreply.github.com>
2018-10-31 15:28:03 +0100

    LancerDeInitial

--- MonopolyGenetiqueJava/src/Metier/Automate/Des/LanceDesInitial.java
+++ MonopolyGenetiqueJava/src/Metier/Automate/Des/LanceDesInitial.java
@@ -1,9 +1,18 @@
 package Metier.Automate.Des;
+import java.util.Random;
 
 public class LanceDesInitial {
 
     public void LancerDes () {
+        int de1;
+        int de2;
+        Random toto = new Random();
 
+        de1=toto.nextInt(6)+1;
+        de2=toto.nextInt(6)+1;
+
+        System.out.println("Dé1 : "+de1);
+        System.out.println("Dé2 : "+de2);
     }
 
 }
