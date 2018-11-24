04960be16b64888959348b8e6c8d92ed03e5299f
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2018-11-24 11:39:37 +0100

    hjkl

--- MonopolyGenetiqueJava/src/IHM/Plateau/LieuPublic/Depart.java
+++ MonopolyGenetiqueJava/src/IHM/Plateau/LieuPublic/Depart.java
@@ -1,9 +1,13 @@
-package IHM.Plateau.CartePiocher;
+package IHM.Plateau.LieuPublic;
 
 import IHM.Plateau.VueCases;
 import Metier.Plateau.Cases;
 
+import javax.swing.text.html.ImageView;
+
 public class Depart extends VueCases {
+
+    private ImageView depart ;
 
     public Depart(Cases caseMetier) {
         super(caseMetier);
