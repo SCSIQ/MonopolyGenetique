a43299a4ae8382efbc9ca6bda17f55403e0c75e9
Aurian <durand.aurian@me.com>
2018-12-02 17:38:34 +0100

    Commencement implémentation payerLoyer

--- MonopolyGenetiqueJava/src/Metier/Automate/Etats/Deplacement.java
+++ MonopolyGenetiqueJava/src/Metier/Automate/Etats/Deplacement.java
@@ -2,14 +2,16 @@ package Metier.Automate.Etats;
 
 import Entites.Joueur;
 import Metier.Automate.Automate;
+import Metier.Automate.Etats.Argent.PayerLoyer;
 import Metier.Automate.Etats.Choix.ChoixPossibles;
 import Metier.Plateau.Cases;
+import Metier.Plateau.ListeProprietes.ListeTerrains.Terrain;
 
 import java.util.ArrayList;
 
 public class Deplacement extends Etat{
 
-    private boolean passeParDepart = false;
+    private boolean doitPayerLoyer = false;
 
     public Deplacement(Automate automate, ArrayList<Joueur> listeJoueurs) {
         super(automate, listeJoueurs);
@@ -38,7 +40,6 @@ public class Deplacement extends Etat{
         if (newPositionToGo > 39)
         {
             newPositionToGo = newPositionToGo - 40; //Permet de denrepositionner le pion sur le nouveau tour du plateau
-            this.passeParDepart = true;
 
             //si le joueur courant s'arrête sur la case départ précissement il gagne 3 000€
             if(newPositionToGo==0){
@@ -50,11 +51,28 @@ public class Deplacement extends Etat{
         j.Avancer(newPositionToGo); //demande au joueur d'avancer son pion (le joueur connais les résultats de son lancé de dès
 
         System.out.println(j.toString());
+
+        //si la case est un terrain
+        if(j.getPion().getCase() instanceof Terrain)
+        {
+            //si elle appatien à un joueur
+            if(((Terrain) j.getPion().getCase()).getProprio() != null)
+            {
+                doitPayerLoyer = true;
+            }
+        }
     }
 
     @Override
     public Etat transition(String event) {
-        return new ChoixPossibles(super.getAutomate(), super.getListeJoueurs());
+        if(doitPayerLoyer==true)
+        {
+            return new PayerLoyer(super.getAutomate(), super.getListeJoueurs());
+        }
+        else
+        {
+            return new ChoixPossibles(super.getAutomate(), super.getListeJoueurs());
+        }
     }
 
     @Override
--- MonopolyGenetiqueJava/src/Metier/Plateau/ListeProprietes/ListeServicesPublics/ServicePublic.java
+++ MonopolyGenetiqueJava/src/Metier/Plateau/ListeProprietes/ListeServicesPublics/ServicePublic.java
@@ -4,7 +4,19 @@ import Metier.Plateau.ListeProprietes.Proprietes;
 
 public abstract class ServicePublic extends Proprietes {
 
+    protected int loyer = 200;
+
     public ServicePublic(int position) {
         super(position);
+        super.setPrix(15000);
+        super.setPrixHypotheque(0);
+    }
+
+    public int getLoyer() {
+
+        //ici coder le fait que le loyer dépend du nombre de service public que possède le joueur
+        //ainsi que du résultat des dès du joueur devant payer le loyer
+
+        return loyer;
     }
 }
