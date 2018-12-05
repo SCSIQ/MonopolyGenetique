823a6e169b15548b0f5a3fe94eb912be29a21399
AlexianeIUT <35493278+AlexianeIUT@users.noreply.github.com>
2018-12-05 11:06:14 +0100

    état achatMaison vendreMaion vendrePropriete

--- MonopolyGenetiqueJava/src/Metier/Automate/Etats/Argent/Maison/VendreMaison.java
+++ MonopolyGenetiqueJava/src/Metier/Automate/Etats/Argent/Maison/VendreMaison.java
@@ -2,7 +2,10 @@ package Metier.Automate.Etats.Argent.Maison;
 
 import Entites.Joueur;
 import Metier.Automate.Automate;
+import Metier.Automate.Etats.Choix.ChoixPossibles;
 import Metier.Automate.Etats.Etat;
+import Metier.Plateau.Cases;
+import Metier.Plateau.ListeProprietes.ListeTerrains.Terrain;
 
 import java.util.ArrayList;
 
@@ -12,22 +15,38 @@ public class VendreMaison extends Etat {
 
     public VendreMaison(Automate automate, ArrayList<Joueur> listeJoueurs) {
         super(automate, listeJoueurs);
+        //permet de demander à l'automate d'évoluer une fois du plus de façon automatique
+        super.getAutomate().setAutomatedEvolution(true);
     }
 
-
-    public void ventenMaison(){
-        //ici on enlève une maison
-    }
 
 
     @Override
     public void agir(String event) {
+        Joueur joueurCourant = super.getAutomate().getJoueurCourant();
+        Cases caseCourante = super.getAutomate().getJoueurCourant().getPion().getCase();
+        int prixMaison = ((Terrain) caseCourante).getPrixAjoutMaison() ;
 
+        if (caseCourante instanceof Terrain) {
+
+            //si le propriétaire de la propriété est le joueur courant
+            if (((Terrain) caseCourante).getProprio() == joueurCourant) {
+
+                //si il est possible de vendre une maison
+                if (((Terrain) caseCourante).getNbMaisons() >0 ) {
+                    joueurCourant.IncrementerSolde(prixMaison);
+                    ((Terrain) caseCourante).supprimerMaison();
+                    System.out.println("    Maison vendu : " + caseCourante.toString() + " au prix de " + ((Terrain) caseCourante).getPrixAjoutMaison() + "€");
+                    System.out.println("    Argent restant pour le joueur : " + joueurCourant.getSolde() + "€");
+
+                }
+            }
+        }
     }
 
     @Override
     public Etat transition(String event) {
-        return null;
+        return new ChoixPossibles(super.getAutomate(), super.getListeJoueurs());
     }
 
     @Override
--- MonopolyGenetiqueJava/src/Metier/Automate/Etats/Argent/Proprietes/VendrePropriete.java
+++ MonopolyGenetiqueJava/src/Metier/Automate/Etats/Argent/Proprietes/VendrePropriete.java
@@ -1,10 +1,48 @@
-package Metier.Automate;
+package Metier.Automate.Etats.Argent.Proprietes;
 
+import Entites.Joueur;
+import Metier.Automate.Automate;
+import Metier.Automate.Etats.Choix.ChoixPossibles;
+import Metier.Automate.Etats.Etat;
+import Metier.Plateau.Cases;
 import Metier.Plateau.ListeProprietes.Proprietes;
 
-public class VendrePropriete {
+import java.util.ArrayList;
 
-    public void Vendre(Proprietes p ){
+public class VendrePropriete extends Etat {
 
+    public VendrePropriete(Automate automate, ArrayList<Joueur> listeJoueurs) {
+        super(automate, listeJoueurs);
+        //permet de demander à l'automate d'évoluer une fois du plus de façon automatique
+        super.getAutomate().setAutomatedEvolution(true);
     }
+
+    @Override
+    public void agir(String event) {
+        Joueur joueurCourant = super.getAutomate().getJoueurCourant();
+        Cases caseCourante = super.getAutomate().getJoueurCourant().getPion().getCase();
+        int prixCase = ((Proprietes) caseCourante).getPrix();
+
+        // si la caseCourante est une instance de Propriété
+        if(caseCourante instanceof Proprietes){
+
+            //si le propriétaire de la propriété est le joueur courant
+            if(((Proprietes) caseCourante).getProprio() == joueurCourant){
+                ((Proprietes) caseCourante).setProprio(joueurCourant);
+                joueurCourant.IncrementerSolde(prixCase);
+                joueurCourant.supprimerPropriete((Proprietes) caseCourante);
+                System.out.println("    Propriété vendu: "+caseCourante.toString()+" au prix de "+prixCase+"€");
+                System.out.println("    Argent restant pour le joueur : "+joueurCourant.getSolde()+"€");
+                }
+            }
+        }
+
+
+    @Override
+    public Etat transition(String event) {
+        return new ChoixPossibles(super.getAutomate(), super.getListeJoueurs());
+    }
+
+    @Override
+    public String toString() { return "VendrePropriete{}";  }
 }
