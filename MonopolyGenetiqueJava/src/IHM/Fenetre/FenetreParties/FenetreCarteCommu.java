f53de984277e82d444349203e9d22009aecef003
BrightHammer21 <lucieboucquemont@hotmail.fr>
2018-12-12 15:17:26 +0100

    fenetreCarteCommu

--- MonopolyGenetiqueJava/src/IHM/Fenetre/FenetreParties/FenetreCarteCommu.java
+++ MonopolyGenetiqueJava/src/IHM/Fenetre/FenetreParties/FenetreCarteCommu.java
@@ -1,13 +1,21 @@
 package IHM.Fenetre.FenetreParties;
 
+import IHM.Fenetre.FenetreParties.ComposantPlateau.PasAssezArgent;
 import IHM.Fenetre.FenetreParties.ComposantPlateau.ZoneAdversaires;
 import IHM.Fenetre.FenetreParties.ComposantPlateau.ZoneInfoJoueur;
 import Metier.Automate.Automate;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class FenetreCarteCommu extends Parent {
 
@@ -22,6 +30,53 @@ public class FenetreCarteCommu extends Parent {
         this.canvas=canvas;
         this.zoneJoueur=zoneJoueur;
         this.zoneAd=zoneAd;
+
+        //TEXTE
+        Label l_Commu = new Label("CAISSE DE COMMUNAUTE");
+        l_Commu.setLayoutX(10);
+        l_Commu.setLayoutY(15);
+
+        //BOUTON
+        Button bt_ok = new Button("Ok");
+
+        bt_ok.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //on rend la bonne opacité à la fenêtre
+                detruireCanvas(canvas);
+
+                //met à jour l'argent du joueur courant dans IHM
+                zoneJoueur.SupprimerJoueur();
+                zoneJoueur.genereInfosJoueur(automate);
+
+                zoneAd.SupprimerAdversaire();
+                zoneAd.genererAdversaire(automate,fenetre_actuelle);
+
+
+                //on ferme la fenêtre
+                fenetre_actuelle.close();
+
+            }
+        });
+
+////////EMPECHE LA FENETRE D'ETRE FERMEE TANT QUE L'USER NE CLIQUE PAS SUR UN BOUTON
+        fenetre_actuelle.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                event.consume();
+            }
+        });
+
+/////////TAILLE MIN ET MAX DE LA FENETRE
+        fenetre_actuelle.setMinHeight(220);
+        fenetre_actuelle.setMinWidth(420);
+
+        fenetre_actuelle.setMaxHeight(220);
+        fenetre_actuelle.setMaxWidth(420);
+
+/////////////////////////////////////////////////AJOUT
+        this.getChildren().add(l_Commu);
+        this.getChildren().add(bt_ok);
 
     }
 
