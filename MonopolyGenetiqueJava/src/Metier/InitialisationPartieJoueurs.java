3923a7515bb937c45a0cb2db24ceaacde3fbb17c
Aurian <durand.aurian@me.com>
2018-12-16 17:33:37 +0100

    Correctif

--- MonopolyGenetiqueJava/src/Metier/InitialisationPartieJoueurs.java
+++ MonopolyGenetiqueJava/src/Metier/InitialisationPartieJoueurs.java
@@ -7,9 +7,4 @@ import java.util.ArrayList;
 
 public class InitialisationPartieJoueurs extends InitialisationPartie{
 
-    private void creationListeJoueurs(int nombre, ArrayList<Color> listeCouleurs){
-        for(int i=0 ; i<nombre ; i++){
-            super.listeJoueurs.add(new Humain(super.listeCases, listeCouleurs.get(i)));
-        }
-    }
 }
