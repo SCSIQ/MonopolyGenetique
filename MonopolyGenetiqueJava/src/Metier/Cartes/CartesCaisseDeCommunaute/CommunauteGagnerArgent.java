06e3d631e260c9081df81b83843619d1711ea92f
AlexianeIUT <35493278+AlexianeIUT@users.noreply.github.com>
2018-12-19 11:29:08 +0100

    création de la 1ère carte communauté

---
+++ MonopolyGenetiqueJava/src/Metier/Cartes/CartesCaisseDeCommunaute/CommunauteGagnerArgent.java
@@ -0,0 +1,14 @@
+package Metier.Cartes.CartesCaisseDeCommunaute;
+
+import Metier.Automate.Automate;
+
+public class CommunauteGagnerArgent extends CaisseDeCommunaute{
+
+    public CommunauteGagnerArgent() {    }
+
+    @Override
+    public void agir(Automate automate) {
+        automate.getJoueurCourant().IncrementerSolde(2000); //gagner 2 000€
+        automate.setContenuCartePiochée("Recevez 2 000€ pour votre remarquable performance à la Féérie Dansante des Sirènes.");
+    }
+}
