f44c84610ed4bc666a46ea172a6fdbc45db4ec84
Aurian <durand.aurian@me.com>
2018-12-19 10:52:36 +0100

    Correctif carte chance métier

--- MonopolyGenetiqueJava/src/Metier/Cartes/CartesChances/Chance.java
+++ MonopolyGenetiqueJava/src/Metier/Cartes/CartesChances/Chance.java
@@ -1,9 +1,12 @@
 package Metier.Cartes.CartesChances;
 
+import Metier.Automate.Automate;
 import Metier.Cartes.Cartes;
 
 public abstract class Chance extends Cartes {
 
     public Chance() {
     }
+
+    public abstract void  agir(Automate automate);
 }
