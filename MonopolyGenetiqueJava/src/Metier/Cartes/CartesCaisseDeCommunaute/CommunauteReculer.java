da6dd5f6a2e5adb84b7156a988beb042d9fcf3ae
AlexianeIUT <35493278+AlexianeIUT@users.noreply.github.com>
2018-12-19 11:41:01 +0100

    début de la 2ème carte communauté

---
+++ MonopolyGenetiqueJava/src/Metier/Cartes/CartesCaisseDeCommunaute/CommunauteReculer.java
@@ -0,0 +1,15 @@
+package Metier.Cartes.CartesCaisseDeCommunaute;
+
+import Metier.Automate.Automate;
+
+public class CommunauteReculer extends CaisseDeCommunaute {
+
+    public CommunauteReculer() { super.setTexte("Allez sur la case Avenue Matignon ");
+    }
+    @Override
+    public void agir(Automate automate) {
+
+    }
+
+
+}
