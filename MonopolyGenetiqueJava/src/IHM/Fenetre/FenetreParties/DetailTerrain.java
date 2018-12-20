bf06884723e6f8ff0dd55b0bada6ca1b89e80f38
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2018-12-20 15:36:56 +0100

    Bouton vendre : essai ... echec

--- MonopolyGenetiqueJava/src/IHM/Fenetre/FenetreParties/DetailTerrain.java
+++ MonopolyGenetiqueJava/src/IHM/Fenetre/FenetreParties/DetailTerrain.java
@@ -360,7 +360,9 @@ public class DetailTerrain extends Parent {
         bt_Vendre.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-
+                automate.evoluer("VendrePropriete");
+                poss.getListeTerrains().remove(numBouton);
+                poss.afficherTerrain(automate);
             }
         });
 
