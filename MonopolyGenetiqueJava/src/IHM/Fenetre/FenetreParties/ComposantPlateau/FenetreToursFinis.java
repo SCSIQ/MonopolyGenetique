ed1ab21070d867ed9fd1acba0ec1fdddc5e7d127
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2018-12-26 21:37:22 +0100

    gjvhbjnk

---
+++ MonopolyGenetiqueJava/src/IHM/Fenetre/FenetreParties/ComposantPlateau/FenetreToursFinis.java
@@ -0,0 +1,13 @@
+package IHM.Fenetre.FenetreParties.ComposantPlateau;
+
+import javafx.scene.Parent;
+import javafx.scene.canvas.Canvas;
+import javafx.stage.Stage;
+
+public class FenetreToursFinis extends Parent {
+
+    public FenetreToursFinis(Stage fenetre, Canvas canvas)
+    {
+        
+    }
+}
