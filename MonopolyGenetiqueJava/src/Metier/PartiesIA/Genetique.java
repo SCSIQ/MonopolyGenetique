63eea0f87468459cc7e5bb50f4b87959f68e0f7f
Papydragon <esteark@gmail.com>
2019-01-05 18:55:02 +0100

    Mise en place de la classse Génétique

--- MonopolyGenetiqueJava/src/Metier/PartiesIA/Genetique.java
+++ MonopolyGenetiqueJava/src/Metier/PartiesIA/Genetique.java
@@ -1,7 +1,34 @@
 package Metier.PartiesIA;
 
-public class Genetique {
+import java.util.ArrayList;
+import Metier.PartiesIA.CentreDecision;
 
+
+public class Genetique{
+
+    private ArrayList<CentreDecision> listDM = new ArrayList<>();
+
+    public void setListDM(ArrayList<CentreDecision> listDM) {
+        this.listDM = listDM;
+    }
+
+    public ArrayList<CentreDecision> getListDM() {
+        return listDM;
+    }
+
+    public Genetique(int nbIA, int nbMut) {
+
+        creationDM(nbIA);
+
+    }
+
+    private void creationDM(int nbIA)
+    {
+        for(int i=0;i<nbIA;i++)
+        {
+            listDM.add(new CentreDecision());
+        }
+    }
 
 
 }
