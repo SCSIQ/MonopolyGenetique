e6891f624c09032d7f79f6372729e7eeedf51064
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2019-02-27 10:33:38 +0100

    création d'une classe ComparateurIA
    Permettera de comparer les IA ensemble afin d'établir une selection

---
+++ MonopolyGenetiqueJava/src/Metier/PartiesIA/ComparerIA/ComparateurIA.java
@@ -0,0 +1,12 @@
+package Metier.PartiesIA.ComparerIA;
+
+import Entites.IA;
+
+import java.util.Comparator;
+
+public class ComparateurIA implements Comparator<IA> {
+    @Override
+    public int compare(IA ia, IA t1) {
+        return 0;
+    }
+}
