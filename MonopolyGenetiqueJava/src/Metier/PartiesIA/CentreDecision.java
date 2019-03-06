0ce17fd802d057a9c4d6ef4543263e2af78f11e8
Papydragon <esteark@gmail.com>
2019-03-06 08:51:54 +0100

    Ajout d'état dans l'automate de l'IA

--- MonopolyGenetiqueJava/src/Metier/PartiesIA/CentreDecision.java
+++ MonopolyGenetiqueJava/src/Metier/PartiesIA/CentreDecision.java
@@ -1,6 +1,8 @@
 package Metier.PartiesIA;
 
 import Entites.IA;
+import Metier.Plateau.ListeProprietes.Proprietes;
+import Metier.PartiesIA.PartieIA;
 
 public class CentreDecision {
 
@@ -27,7 +29,6 @@ public class CentreDecision {
         if(this.ia!=null)
         {
 
-
             switch(action)
             {
                 //<editor-fold desc="Case fin de tour">
@@ -36,8 +37,38 @@ public class CentreDecision {
                     break;
                 //</editor-fold>
 
-                //<editor-fold desc="Case propriétés">
+                //<editor-fold desc="Case Achat propriétés">
                 case "propriété":
+
+                    break;
+                //</editor-fold>
+
+                //<editor-fold desc="Case ">
+                case "":
+
+                    break;
+                //</editor-fold>
+
+                //<editor-fold desc="Case ">
+                case "1":
+
+                    break;
+                //</editor-fold>
+
+                //<editor-fold desc="Case ">
+                case "2":
+
+                    break;
+                //</editor-fold>
+
+                //<editor-fold desc="Case ">
+                case "3":
+
+                    break;
+                //</editor-fold>
+
+                //<editor-fold desc="Case passer le tour">
+                    default:
 
                     break;
                 //</editor-fold>
