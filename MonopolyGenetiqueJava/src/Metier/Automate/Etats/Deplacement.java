fc76d95e14a7c861b956fef9f559495befefc1e7
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2019-03-07 15:56:04 +0100

    commit sans forçage du déplacement et de la carte chance

--- MonopolyGenetiqueJava/src/Metier/Automate/Etats/Deplacement.java
+++ MonopolyGenetiqueJava/src/Metier/Automate/Etats/Deplacement.java
@@ -85,8 +85,8 @@ public class Deplacement extends Etat{
         {
             estSurChance = true ; //active la transition à l'état faisant piocher les cartes chances
                 Random rand = new Random();
-               // int resRand = rand.nextInt(5);
-                int resRand=3;
+                int resRand = rand.nextInt(5);
+               // int resRand=3;
                 getAutomate().setContenuCartePiochée(getAutomate().getListeDesCartesChances().get(resRand).getTexte());
                 getAutomate().setTirerCarteChance(resRand);
                 System.out.println("        Carte chance : "+getAutomate().getContenuCartePiochée());
@@ -111,8 +111,8 @@ public class Deplacement extends Etat{
     public void agir(String event) {
         Joueur j = super.getListeJoueurs().get(0);
 
-        //int resDes = j.getResLanceDes();
-        int resDes = 7; //POUR IHM
+        int resDes = j.getResLanceDes();
+        //int resDes = 7; //POUR IHM
 
         Cases position = j.getPion().getCase();
 
