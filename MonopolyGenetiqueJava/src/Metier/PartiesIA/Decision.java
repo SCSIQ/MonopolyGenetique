cc52a936e36bd18c734d6738a00caf4b3cc380d6
BrightHammer21 <lucieboucquemont@hotmail.fr>
2019-03-19 17:35:03 +0100

    decision

---
+++ MonopolyGenetiqueJava/src/Metier/PartiesIA/Decision.java
@@ -0,0 +1,43 @@
+package Metier.PartiesIA;
+
+import Entites.IA;
+
+public class Decision {
+
+    private IA ia;
+
+    public Decision(IA ia)
+    {
+        this.ia=ia;
+    }
+
+    public int estCequeJaiAssezArgent()
+    {
+        int res = 0;
+
+        if(ia.getSolde()>=1000)
+        {
+            res=1;
+        }
+
+        return res;
+    }
+
+    public int aCarteLiberePrison()
+    {
+        int res = 0;
+
+        if(ia.getNbCartesLibereDePrison()>0)
+        {
+            res=1;
+        }
+
+        return res;
+    }
+
+    public int noteCase()
+    {
+        int res = 0;
+        return res;
+    }
+}
